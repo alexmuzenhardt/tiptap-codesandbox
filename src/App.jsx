c94c0eb2a0674ae5c0990902567f78ee3582803d
Alexander Muzenhardt <alexander.muzenhardt@cit.de>
2023-10-09 13:08:18 +0200

    Make the example simpler

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import {useState} from "react";
 import RichtextEditor from "./components/RichtextEditor.jsx";
 
 function App() {
-	const [appleEditorValue, setAppleEditorValue] = useState('apple')
-	const [bananaEditorValue, setBananaEditorValue] = useState('banana')
-	const [selectedEditor, setSelectedEditor] = useState('appleEditor');
+    const [appleEditorValue, setAppleEditorValue] = useState('apple')
+    const [bananaEditorValue, setBananaEditorValue] = useState('banana')
+    const [selectedEditor, setSelectedEditor] = useState('appleEditor');
 
-	return (
-		<main>
-			<button onClick={() => setSelectedEditor('appleEditor')}>Show Apple Editor</button>
-			<button onClick={() => setSelectedEditor('bananaEditor')}>Show Banana Editor</button>
+    return (
+        <main>
+            <button onClick={() => setSelectedEditor('appleEditor')}>Show Apple Editor</button>
+            <button onClick={() => setSelectedEditor('bananaEditor')}>Show Banana Editor</button>
 
-			{selectedEditor === 'appleEditor' ? (
-				<RichtextEditor value={appleEditorValue} setValue={setAppleEditorValue}/>) : (
-				<RichtextEditor value={bananaEditorValue} setValue={setBananaEditorValue}/>
-			)}
-		</main>
-	)
+            {selectedEditor === 'appleEditor' ? (
+                <RichtextEditor key="apple" value={appleEditorValue} setValue={setAppleEditorValue}/>) : (
+                <RichtextEditor key="banana" value={bananaEditorValue} setValue={setBananaEditorValue}/>
+            )}
+            {/*    <RichtextEditor value={appleEditorValue} setValue={setAppleEditorValue}/>) : (*/}
+            {/*    <RichtextEditor value={bananaEditorValue} setValue={setBananaEditorValue}/>*/}
+        </main>
+    )
 }
 
 export default App
--- src/components/RichtextEditor.jsx
+++ src/components/RichtextEditor.jsx
@@ -1,64 +1,36 @@
-import {useState} from 'react';
 import {StarterKit} from "@tiptap/starter-kit";
 import {BubbleMenu, EditorContent, useEditor} from "@tiptap/react";
 
 // eslint-disable-next-line react/prop-types
-const RichtextEditor = ({value, setValue}) => {
-	const [showSecondEditor, setShowSecondEditor] = useState(false);
+const RichtextEditor = ({value, setValue, key}) => {
+    const extensions = [
+        StarterKit,
+    ]
 
-	const extensions = [
-		StarterKit,
-	]
+    const content = value;
 
-	const content = value;
+    const editor = useEditor({
+        extensions,
+        content,
 
-	const editor1 = useEditor({
-		extensions,
-		content,
+        onUpdate({editor}) {
+            setValue(editor.getHTML());
+        }
+    })
 
-		onUpdate({editor}) {
-			setValue(editor.getHTML());
-			editor2.commands.setContent(editor.getHTML());
-		}
-	})
+    console.log(value);
 
-	const editor2 = useEditor({
-		extensions,
-		content,
+    return (
+        <section>
+            <h2>Editor</h2>
 
-		onUpdate({editor}) {
-			setValue(editor.getHTML());
-			editor1.commands.setContent(editor.getHTML());
-		}
-	})
+            <EditorContent editor={editor}/>
 
-	console.log('VALUE: ', value);
-
-	return (
-		<>
-			<section>
-				<p>First Editor</p>
-
-				<EditorContent editor={editor1}/>
-
-				<BubbleMenu editor={editor1}>
-					<button onClick={() => editor1.chain().toggleBold().focus().run()}>bold</button>
-				</BubbleMenu>
-			</section>
-
-			<section>
-				<button onClick={() => setShowSecondEditor(!showSecondEditor)}>Toggle second Editor</button>
-			</section>
-
-			{showSecondEditor ? (
-				<section>
-					<p>Second Editor</p>
-
-					<EditorContent editor={editor2}/>
-				</section>
-			) : null}
-		</>
-	);
+            <BubbleMenu editor={editor}>
+                <button onClick={() => editor.chain().toggleBold().focus().run()}>bold</button>
+            </BubbleMenu>
+        </section>
+    );
 };
 
 export default RichtextEditor;
